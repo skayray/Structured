7d8291926735aecc97198396446e1895426a88f8
skayray <98284401+skayray@users.noreply.github.com>
2025-09-10 16:13:36 +0200

    Update svg-support.php

--- template-functions/svg-support.php
+++ template-functions/svg-support.php
@@ -3,7 +3,7 @@ function cc_mime_types($mimes) {
 	if ( current_user_can( 'administrator' ) ) {
   $mimes['svg'] = 'image/svg+xml';
   $mimes['lottie'] = 'application/zip+dotlottie';
-  return $mimes;
 }
+  return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
